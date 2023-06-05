639ba6a67300e83cee3736c9112e99b44c627df1
JoanaCasco <joanacascogalea@gmail.com>
2023-06-05 12:39:18 +0200

    sincronizar

--- src/control/LoginListener.java
+++ src/control/LoginListener.java
@@ -3,55 +3,69 @@ package control;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JMenuItem;
+
 import db.UsuarioPersistencia;
 import model.User;
 import view.VLogin;
+import view.VPrincipal;
 
-public class LoginListener implements ActionListener{
-	
+public class LoginListener implements ActionListener {
+
 	private VLogin vLogin;
 	private UsuarioPersistencia uPers;
-	
+	private VPrincipal vp;
 
-	public LoginListener(VLogin vLogin) {
-		
+	public LoginListener(VLogin vLogin, VPrincipal vp) {
+
 		this.vLogin = vLogin;
+		this.vp = vp;
 		uPers = new UsuarioPersistencia();
-		
+
 	}
-
-
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		if (e.getActionCommand().equals(VLogin.BTN_ACCEDER)) {
-			
+
 			User user = vLogin.getUsuario();
-			
+
 			if (user != null) {
-				
+
 				String resultado = uPers.validarUsuario(user);
-				
+
 				if (resultado.equals("Acceso permitido")) {
-					
-					//vLogin.mostrarError(resultado);
-					
+
+					// vLogin.mostrarError(resultado);
+
 				} else {
-					
+
 					vLogin.mostrarError(resultado);
-					
+
 				}
-				
+
 			}
-			
+
 		} else if (e.getActionCommand().equals(VLogin.BTN_RESGISTRAR)) {
-			
-			//TODO
+
+			// TODO
 			// ABRIR VRegistroUser AL PULSAR BOTÓN
-			
+
+		} else if (e.getSource() instanceof JMenuItem) {
+
+			if (e.getActionCommand().equals(VPrincipal.MNTM_INICIO)) {
+
+			} else if (e.getActionCommand().equals(VPrincipal.MNTM_HORARIOS)) {
+
+			} else if (e.getActionCommand().equals(VPrincipal.MNTM_RESERVASCLASES)) {
+
+			} else if (e.getActionCommand().equals(VPrincipal.MNTM_RESERVASPISTAS)) {
+
+			} else if (e.getActionCommand().equals(VPrincipal.MNTM_SALIR)) {
+				vp.confirmarSalida();
+			}
+
 		}
-
 	}
-
 }
--- src/main/Inicio.java
+++ src/main/Inicio.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 
 import control.LoginListener;
 import view.VLogin;
+import view.VPrincipal;
 
 public class Inicio {
 	
@@ -16,11 +17,15 @@ public class Inicio {
 				
 				VLogin vLogin = new VLogin();
 				
-				LoginListener listener = new LoginListener(vLogin);
+				VPrincipal vp = new VPrincipal();
+				
+				LoginListener listener = new LoginListener(vLogin, vp);
 				
 				vLogin.setListener(listener);
 				
 				vLogin.hacerVisible();
+				
+				vp.setListener(listener);
 				
 			}
 		});
