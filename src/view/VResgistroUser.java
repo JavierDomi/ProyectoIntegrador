1aeb3cda0cb99bc006c73dcb5c46e35a42fbe8d9
Liana Vilkelenoka <lvilkelenoka@gmail.com>
2023-06-05 11:08:32 +0200

    VRegistro y creacion de tabla User BBDD

--- src/view/VResgistroUser.java
+++ src/view/VResgistroUser.java
@@ -19,132 +19,126 @@ import javax.swing.Icon;
 import javax.swing.JButton;
 
 public class VResgistroUser extends JFrame{
-	
-	private static final int ANCHO = 500;
+
+	private static final int ANCHO = 400;
 	private static final int ALTO = 250;
-	
-	public static final String BTN_ACCEDER = "Acceder";
-	
+	public static final String BTN_GUARDAR = "Guardar";
+	public static final String BTN_CANCELAR = "Cancelar";
 	private JTextField txtUser;
 	private JPasswordField pswPassword;
-	private JButton btnRegistrar;
-	private JPasswordField pswConPassword;
+	private JButton btnGuardar;
+	private JButton btnCancelar;
+	private JPasswordField pswPassword2;
+	private JLabel lblConfirmarPassw;
 
 	public VResgistroUser() {
-		
-		super ("Registro Usuario");
-		setTitle("Registro Usuario");
-		centrarVentana();
+		super("Registro Usuario");
 		init();
 		
 	}
 
 	private void init() {
-		
 		getContentPane().setLayout(null);
 		
-		JLabel lblUser = new JLabel("User");
-		lblUser.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblUser.setBounds(52, 32, 75, 20);
-		getContentPane().add(lblUser);
+		JLabel lblUsuario = new JLabel("User");
+		lblUsuario.setBounds(30, 30, 75, 20);
+		getContentPane().add(lblUsuario);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblPassword.setBounds(52, 72, 75, 20);
+		lblPassword.setBounds(30, 62, 75, 20);
 		getContentPane().add(lblPassword);
 		
 		txtUser = new JTextField();
 		txtUser.setToolTipText("Introduce tu usuario");
-		txtUser.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		txtUser.setBounds(269, 30, 162, 25);
+		txtUser.setBounds(183, 27, 182, 25);
 		getContentPane().add(txtUser);
 		txtUser.setColumns(10);
 		
 		pswPassword = new JPasswordField();
 		pswPassword.setToolTipText("Introduce tu password");
-		pswPassword.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		pswPassword.setBounds(269, 70, 162, 25);
+		pswPassword.setBounds(183, 59, 182, 26);
 		getContentPane().add(pswPassword);
 		
-		btnRegistrar = new JButton("Registrar");
-		btnRegistrar.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnRegistrar.setBounds(269, 159, 100, 25);
-		getContentPane().add(btnRegistrar);
+		btnGuardar = new JButton(BTN_GUARDAR);
+		btnGuardar.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		btnGuardar.setBounds(226, 158, 117, 29);
+		getContentPane().add(btnGuardar);
 		
-		pswConPassword = new JPasswordField();
-		pswConPassword.setToolTipText("Introduce tu password");
-		pswConPassword.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		pswConPassword.setBounds(269, 109, 162, 25);
-		getContentPane().add(pswConPassword);
+		btnCancelar = new JButton(BTN_CANCELAR);
+		btnCancelar.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		btnCancelar.setBounds(62, 158, 117, 29);
+		getContentPane().add(btnCancelar);
 		
-		JLabel lblConPassword = new JLabel("Confirmar Password");
-		lblConPassword.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblConPassword.setBounds(52, 114, 128, 20);
-		getContentPane().add(lblConPassword);
+		lblConfirmarPassw = new JLabel("Confirm password");
+		lblConfirmarPassw.setBounds(30, 102, 149, 20);
+		getContentPane().add(lblConfirmarPassw);
+		
+		pswPassword2 = new JPasswordField();
+		pswPassword2.setToolTipText("Introduce tu password");
+		pswPassword2.setBounds(183, 99, 182, 26);
+		getContentPane().add(pswPassword2);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
-		setSize(ANCHO, ALTO);
+		setSize(ANCHO,ALTO);
 		centrarVentana();
 		
+		
 	}
-
 	private void centrarVentana() {
-		   
-		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();                   
-		Dimension ventana = new Dimension(ANCHO, ALTO);                     
+		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();               
+		Dimension ventana = new Dimension(ANCHO, ALTO);               
 		setLocation((pantalla.width - ventana.width) / 2,  (pantalla.height - ventana.height) / 2); 
 		
 	}
-	
 	public void hacerVisible() {
-		
 		setVisible(true);
-		
 	}
-
+	
 	public void setListener(LoginListener listener) {
-
-		btnRegistrar.addActionListener(listener);
-		
+		btnGuardar.addActionListener(listener);
+		btnCancelar.addActionListener(listener);
 	}
 
 	public User getUsuario() {
+		User user = null;
 		
-		String user = txtUser.getText();
-		User usuario = null;
+		String usuario = txtUser.getText().trim();
+	
 		
-		if (user.isEmpty()) {
-			
+		if(usuario.isEmpty()) {
 			mostrarError("Debe introducir el usuario");
-			
-		} else {
-			
-			
+		}else {
 			String pwd = pswPassword.getText().trim();
-			String pwdCon = pswConPassword.getText().trim();
+			String pwd2 = pswPassword2.getText().trim();
 			
-			if (pwd.isEmpty() || pwdCon.isEmpty()) 
-				
-				mostrarError("Debe introducir la contraseña en ambos campos");
-			
-			else if (!pwd.equals(pwdCon)){
-				
-				mostrarError("La contraseña de verificación no coincide");
-				
-			} else {
-				
-				usuario = new User(user, pwd);
+			if(pwd.isEmpty() || pwd2.isEmpty()) {
+				mostrarError("Debe introducir el password y la confirmación");
+			}else {
+				if(!pwd.equals(pwd2)) {
+					mostrarError("Las contraseñas no coinciden");
+				}else {
+					user = new User(usuario, pwd);
+				}
 				
 			}
-			
 		}
 		
-		return usuario;
+		
+		
+		return user;
 	}
 
 	public void mostrarError(String error) {
-		
 		JOptionPane.showMessageDialog(this, error, "ERROR", JOptionPane.ERROR_MESSAGE);
+		
+	}
+	public void mostrarMensaje(String texto) {
+		JOptionPane.showMessageDialog(this, texto, "Infromacion", JOptionPane.INFORMATION_MESSAGE);
+		
+	}
+
+	public void mostrarMensaje(String texto, String titulo, int icono) {
+		JOptionPane.showMessageDialog(this,texto, titulo, icono);
 		
 	}
 }
